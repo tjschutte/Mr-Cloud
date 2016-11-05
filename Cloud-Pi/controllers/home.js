f88d7124944e3e5a7ae02cdebe2a6e4497407b31
Tom Schutte <tjschutte@wisc.edu>
2016-11-04 20:54:42 -0500

    got posts working, attempting to get serial working
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Cloud-Pi/controllers/home.js
+++ Cloud-Pi/controllers/home.js
@@ -4,4 +4,9 @@
  */
 exports.index = (req, res) => {
   res.render('home.html');
+  console.log('Hey! Mind your own buisness!');
 };
+
+exports.postIndex = (req, res) => {
+  console.log('There was a post from the index page!');
+}
--- Cloud-Pi/index.js
+++ Cloud-Pi/index.js
@@ -3,10 +3,24 @@
  */
 const express = require('express');
 
+/*
+ * Some serialport initialization stuff first
+ */
+var serialport = require('serialport'),// include the library
+SerialPort = serialport.SerialPort,    // make a local instance of it
+portName = '/dev/tty0';
+
+var data = '2\n';                          // latest data
+var servi = require('servi');          // include the servi library
+var myPort = new SerialPort(portName, {
+  baudRate: 9600
+});
+
 /**
  * Controllers (route handlers).
  */
 const homeController = require('./controllers/home');
+
 
 /**
  * Create Express server.
@@ -20,6 +34,13 @@ app.engine('html', require('ejs').renderFile);
  * Primary app routes.
  */
 app.get('/', homeController.index);
+app.post('/', homeController.postIndex);
+app.route('/data', sendToSerial);
+
+function sendToSerial(data) {
+  console.log("sending to serial: " + data);
+  myPort.write(data);
+}
 
 /**
  * Start Express server.
