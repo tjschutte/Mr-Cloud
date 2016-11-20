d6d8b1435f5d2d3d6c32a0268a65053fd694046b
Tom Schutte <tjschutte@wisc.edu>
2016-11-20 16:50:10 +0000

    Added in all the supported modes that the light strip currently has.
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Cloud-Pi/controllers/home.js
+++ Cloud-Pi/controllers/home.js
@@ -8,7 +8,7 @@ const proc = require('child_process').spawn;
  */
 exports.index = (req, res) => {
   res.render('home.html');
-  console.log('Hey! Mind your own buisness!');
+  console.log('GET: /index');
 };
 
 /*
@@ -16,17 +16,8 @@ exports.index = (req, res) => {
   Home page.
 */
 exports.postIndex = (req, res) => {
-  console.log('Updating mode');
-  console.log(req.body.mode.mode);
-  res.render('home.html');
+  console.log('POST: /index');
+  console.log('Changing mode to: ' + req.body.mode.mode);
   var p = proc('python2.7', ['cloud.py', req.body.mode.mode]);
-/*
-  p.stdout.on('data', (data) => {
-    console.log('stdout: ' + data);
-   });
-
-  p.on('close', (code) => {
-    console.log('Exited with code: ' + code);
-  });
-*/
+  res.render('home.html');
 }
