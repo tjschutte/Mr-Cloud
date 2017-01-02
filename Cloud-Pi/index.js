f2c6da7c3976d70fcfb1800923703f84c96a7b75
Tom Schutte <tjschutte@wisc.edu>
2017-01-02 17:00:31 -0600

    Tweaks and modifications.
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Cloud-Pi/index.js
+++ Cloud-Pi/index.js
@@ -10,19 +10,20 @@ const port = new serialport('/dev/ttyUSB0', 9600);
 
 // Global variable to turn checking weather on and off
 var getWeather = true;
+var quietHours = true;
 
 // Other configuration variables
 var quietStart = 22;
 var quietEnd = 6;
 
 /* Open weather configuration */
-weather.setAPPID('');
+weather.setAPPID('bb3c7f07ddd4adcbee99aa50c6badef6');
 weather.setCulture('en');
 weather.setForecastType('daily');
 
 /* seconds, minutes, hours, day-of-month, month, day-of-week */
 /* this runs an event every minute */
-var event = schedule.scheduleJob('*/15 * * * * *', function() {
+var event = schedule.scheduleJob('*/60 * * * * *', function() {
   var date = new Date();
   var currentHour = date.getHours();
   var currentMinute = date.getMinutes();
@@ -36,29 +37,28 @@ var event = schedule.scheduleJob('*/15 * * * * *', function() {
   
       if (currentHour == sunrise.getHours() && currentMinute >= sunrise.getMinutes()) {
         console.log('sunrise');
-        data = '1'; //color for sunrise
+        data = '16'; //color for sunrise
       } else if (currentHour == sunset.getHours() && currentMinute >= sunset.getMinutes()) {
         console.log('sunset');
-        data = '-1' //color for sunset
+        data = '17' //color for sunset
       } else {
         // If there has been some rain, lets do some lightning
         console.log(json.values.weather[0].main);
         if (json.values.weather.main == 'Rain') {
           var rand = Math.random();
-          data = (rand < 0.2 ? '0' : '2'); // set to more common lighting, or light blue for rain
-          data = (rand < 0.08 ? '0' : '6'); // set it to lightning, or light blue for rain
+          data = (rand < 0.2 ? '18' : '2'); // set to more common lighting, or light blue for rain
+          data = (rand < 0.08 ? '18' : '6'); // set it to lightning, or light blue for rain
         } else if (json.values.weather[0].main == 'Clear') {
-          data = '1'; // weather is clear, make it sunny
+          data = '15'; // weather is clear, make it sunny
         }
       }
       console.log('Changing mode to: ' + data);
       port.write(data);
     });
   }
-
   // If it is past bedtime, overwrite everything
-  if (currentHour >= quietStart || currentHour <= quietEnd) {
-//    console.log('Lights out');
+  if (quietHours && (currentHour >= quietStart || currentHour <= quietEnd)) {
+    console.log('Lights out');
     port.write('-1');
   }
 
@@ -101,6 +101,10 @@ app.post('/', function postIndex(req, res) {
     getWeather = false;
   else if (req.body.mode.mode == 1001)
     getWeather = true;
+  else if (req.body.mode.mode == 2000)
+    quietHours = false;
+  else if (req.body.mode.mode == 2001)
+    quietHours = true;
   else
     port.write(req.body.mode.mode);
 
