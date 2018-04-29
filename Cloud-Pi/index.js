f051a40ae232efad0fe2d5ededfe8dc630c3001a
Tom Schutte <tjschutte@wisc.edu>
2018-04-29 02:30:12 +0000

    Adding all packages to packages.json
    Removed unneeded comments and added README with node and npm version
    information
    
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Cloud-Pi/index.js
+++ Cloud-Pi/index.js
@@ -1,6 +1,3 @@
-/**
- * Module dependencies.
- */
 const express = require('express');
 const bodyParser = require('body-parser');
 const serialport = require('serialport');
@@ -66,30 +63,17 @@ var event = schedule.scheduleJob('*/60 * * * * *', function() {
 
 });
 
-/**
- * Create Express server.
- */
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', 80);
 app.set('views', __dirname + '/views');
 app.engine('html', require('ejs').renderFile);
 
-/** bodyParser.urlencoded(options)
-* Parses the text as URL encoded data (which is how browsers tend to send form data from regular forms set to POST)
-* and exposes the resulting object (containing the keys and values) on req.body
-*/
 app.use(bodyParser.urlencoded({
   extended: true
 }));
 
-/**bodyParser.json(options)
-* Parses the text as JSON and exposes the resulting object on req.body.
-*/
 app.use(bodyParser.json());
 
-/**
- * Primary app routes.
- */
 app.get('/', function getIndex(req, res) {
   console.log('GET: /index');
   res.render('home.html');
@@ -137,9 +121,6 @@ app.post('/', function postIndex(req, res) {
   res.render('home.html');
 });
 
-/**
- * Start Express server.
- */
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
 });
