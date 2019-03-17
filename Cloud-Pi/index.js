ce7f4529f44bac6c00e5cea90c363c96cf0f5b21
Tom Schutte <tjschutte@wisc.edu>
2019-03-16 21:21:53 -0700

    Cleaning up / changing location.
    Moved things to a config file, old API key is still present as a placeholder.
    
    Signed-off-by: Tom Schutte <tjschutte@wisc.edu>

--- Cloud-Pi/index.js
+++ Cloud-Pi/index.js
@@ -1,45 +1,63 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const serialport = require('serialport');
 const schedule = require('node-schedule');
 const weather = require('openweather-node');
+const config = require('./config.json');
+var serialport;
 
-const port = new serialport('/dev/ttyUSB0', 9600);
-const sleep = require('sleep');
+Date.prototype.getUnixTime = function() { 
+  return this.getTime()/1000|0 
+};
+if (!Date.now)
+  Date.now = function() { 
+    return new Date(); 
+  }
+Date.time = function() { 
+  return Date.now().getUnixTime(); 
+}
+
+// debug sets some startup features like weather and quiethours to false. 
+// dev mode stops us from doing serial items incase the machine does not have that ability
+var debug = (process.argv[2] == 'debug' || process.argv[3] == 'debug');
+var devMode = (process.argv[2] == 'dev' || process.argv[3] == 'dev');
 
 // Global variable to turn checking weather on and off
-var getWeather = (process.argv[2] == 'debug') ? false : true;
-var quietHours = (process.argv[2] == 'debug') ? false : true;
+var weatherMode = config.weatherMode;
+var quietHours = config.timerMode;
 
 // Other configuration variables
-var quietStart = 22;
-var quietEnd = 6;
+var quietStart = config.quietStart;
+var quietEnd = config.quietEnd;
 
 /* Open weather configuration */
-weather.setAPPID('bb3c7f07ddd4adcbee99aa50c6badef6');
-weather.setCulture('en');
-weather.setForecastType('daily');
+weather.setAPPID(config.weatherAPIKey);
+weather.setCulture(config.weatherCulture);
+weather.setForecastType(config.weatherForecastType);
+
+// Serial port.
+var port = null;
+
+if (!devMode){
+  serialport = require('serialport');
+  // Serial port used to talk to arduino which talks to lights.
+  port = new serialport('/dev/ttyUSB0', 9600);  
+}
 
 /* seconds, minutes, hours, day-of-month, month, day-of-week */
 /* this runs an event every minute */
 var event = schedule.scheduleJob('*/60 * * * * *', function() {
-  var date = new Date();
-  var currentHour = date.getHours();
-  var currentMinute = date.getMinutes();
-  var data;
-  if (getWeather) {
-    console.log('Weather Mode enabled.');
-    weather.now('Madison', function(err, json) {
-      var sunrise = new Date(1000 * json.values.sys.sunrise);
-      var sunset = new Date(1000 * json.values.sys.sunset);
-      data = '0';
+  var data = '0';
 
-      if (currentHour == sunrise.getHours() && currentMinute >= sunrise.getMinutes()) {
-        console.log('sunrise');
-        data = '16'; //color for sunrise
-      } else if (currentHour == sunset.getHours() && currentMinute >= sunset.getMinutes()) {
-        console.log('sunset');
-        data = '17' //color for sunset
+  if (weatherMode) {
+    console.log('Weather Mode enabled.');
+    weather.now(config.location, function(err, json) {
+
+      if (sunEvent(json.values.sys.sunrise)) {
+        console.log('Sunrise!')
+        data = '16'; // color for sunrise
+      } else if (sunEvent(json.values.sys.sunset)) {
+        console.log('Sunset!')
+        data = '17' // color for sunset
       } else {
         // If there has been some rain, lets do some lightning
         console.log(json.values.weather[0].main);
@@ -51,16 +69,17 @@ var event = schedule.scheduleJob('*/60 * * * * *', function() {
           data = '15'; // weather is clear, make it sunny
         }
       }
-      console.log('Changing mode to: ' + data);
-      port.write(data);
     });
   }
   // If it is past bedtime, overwrite everything
-  if (quietHours && (currentHour >= quietStart || currentHour <= quietEnd)) {
-    console.log('Lights out');
-    port.write('-1');
+  if (isQuietHours()) {
+    console.log('Lights out')
+    data = '-1';
   }
-
+  console.log('Changing mode to: ' + data);
+  if (!devMode) {
+    port.write(data);
+  }
 });
 
 const app = express();
@@ -74,52 +93,56 @@ app.use(bodyParser.urlencoded({
 
 app.use(bodyParser.json());
 
+// Get the main page when someone navigates to the server.
 app.get('/', function getIndex(req, res) {
   console.log('GET: /index');
   res.render('home.html');
 });
 
+// Post takes values back from the user and updates the arduino.
 app.post('/', function postIndex(req, res) {
   console.log('POST: /index');
   console.log('Changing mode to: ' + req.body.mode.mode);
 
-  if (req.body.mode.mode == 1000) {
-    getWeather = false;
-    console.log('Turning weather updates off');
-  }
-  else if (req.body.mode.mode == 1001) {
-    getWeather = true;
-    console.log('Turning weather updates on');
-  }
-  else if (req.body.mode.mode == 2000) {
-    quietHours = false;
-    console.log('Turning quiet hours off');
-  }
-  else if (req.body.mode.mode == 2001) {
-    quietHours = true;
-    console.log('Turning quiet hours on');
-  } else if (req.body.mode.mode == 255) {
-    var color = req.body.mode.color;
-    var red = parseInt(color.substring(1, 3), 16);
-    var green = parseInt(color.substring(3, 5), 16);
-    var blue = parseInt(color.substring(5, 7), 16);
-    console.log('custom color: ' + req.body.mode.color);
-    port.write('255');
-    sleep.usleep(100000);
-    port.write(red.toString());
-    console.log('red: ' + red);
-    sleep.usleep(100000);
-    port.write(green.toString());
-    console.log('green: ' + green);
-    sleep.usleep(100000);
-    port.write(blue.toString());
-    console.log('blue: ' + blue);
-  }
-  else {
-    port.write(req.body.mode.mode);
+  if (!devMode) {
+    switch(req.body.mode.mode) {
+      case 1000:
+        weatherMode = false;
+        break;
+      case 1001:
+        weatherMode = true;
+        break;
+      case 2000:
+        break;
+      case 2001:
+        break;
+      default:
+        port.write(req.body.mode.mode);
+        break;
+    }
   }
   res.render('home.html');
 });
+
+// 15 minutes * 60 seconds * 1000 milli seconds. 
+// Used in sunrise / set calculations.
+const sunPeriod = (15 * 60);
+
+// Returns if the time allows for sunrise / sunset
+function sunEvent(time) {
+  var date = (+ new Date()) / 1000;
+  var beforeBuffer = time - sunPeriod;
+  var afterBuffer = time + sunPeriod;
+  if (date > beforeBuffer && date < afterBuffer) {
+    return true
+  }
+  return false;
+};
+
+function isQuietHours() {
+  var hour = new Date().getHours;
+  return hour > quietStart || hour < quietEnd;
+};
 
 app.listen(app.get('port'), () => {
   console.log('Express server listening on port %d in %s mode', app.get('port'), app.get('env'));
